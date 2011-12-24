a613a474a4cd91b70f1b0c40dea8154d52fead99
Geoff Lane <geoff@zorched.net>
2011-12-23 21:43:01 -0500

    Increment version to 0.7
    Signed-off-by: Geoff Lane <geoff@zorched.net>

--- ConstraintsGrailsPlugin.groovy
+++ ConstraintsGrailsPlugin.groovy
@@ -26,10 +26,10 @@ import net.zorched.grails.plugins.validation.CustomConstraintFactory
 
 class ConstraintsGrailsPlugin {
     // the plugin version
-    def version = "0.6.0"
+    def version = "0.7.0"
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.1.1 > *"
+    def grailsVersion = "2.0.0 > *"
 
     // the other plugins this plugin depends on
     def dependsOn = [:]
