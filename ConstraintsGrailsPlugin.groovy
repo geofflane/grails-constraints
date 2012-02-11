5e60a3ad9dbaa4d90a80697154d5838f3772eaf5
Geoff Lane <geoff@zorched.net>
2012-02-10 22:18:52 -0500

    Increment version for new release

--- ConstraintsGrailsPlugin.groovy
+++ ConstraintsGrailsPlugin.groovy
@@ -26,7 +26,7 @@ import net.zorched.grails.plugins.validation.CustomConstraintFactory
 
 class ConstraintsGrailsPlugin {
     // the plugin version
-    def version = "0.7.0"
+    def version = "0.8.0"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
