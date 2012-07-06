7d13bb6a21992b99be1193ebadde425cc5e3a69c
Geoff Lane <geoff@zorched.net>
2012-07-05 21:51:27 -0400

    Upgrade to latest Grails 2.0.3 and test. Set compile to 1.6.
    Signed-off-by: Geoff Lane <geoff@zorched.net>

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,6 +2,7 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir	= "target/test-reports"
 grails.release.scm.enabled = false
+grails.project.source.level = 1.6
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
